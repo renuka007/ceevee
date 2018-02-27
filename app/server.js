75e82dd9c41cc67107c830801bfad8a6328a8965
Randall Morey <randall@randallmorey.com>
2018-02-26 19:13:36 -0500

    added POST /users endpoint and acceptance tests

---
+++ app/server.js
@@ -0,0 +1,39 @@
+import restify from 'restify';
+import { makeConstructor } from 'restify-errors';
+import User from './models/user';
+
+const server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
+
+// /users
+const UserNotCreatedError = makeConstructor('UserNotCreatedError', {
+    statusCode: 422,
+    message: 'User could not be created.'
+});
+server.post('/users', (req, res, next) => {
+  const {email, password} = (req.body || {});
+  const user = new User({email});
+  user.setPassword(password).then((value) => {
+    user.save((err) => {
+      if (err) {
+        // error state
+        // All errors result in the same response.  This helps to prevent
+        // inadvertantly leaking information about which email addresses already
+        // exist in the database, although it is not foolproof.
+        next(new UserNotCreatedError());
+      } else {
+        // success state
+        res.send(201, {
+          email: user.email
+        });
+        next();
+      }
+    });
+  }).catch(() => {
+    // password error state
+    next(new UserNotCreatedError());
+  });
+});
+
+export default server;
---
+++ test/acceptance/users.js
@@ -0,0 +1,82 @@
+import { assert } from 'chai';
+import supertest from 'supertest';
+import server from '../../app/server';
+import User from '../../app/models/user';
+import DatabaseHelper from '../helpers/database-helper';
+
+describe('Acceptance: Route: /users', () => {
+
+  beforeEach(async () => {
+    await DatabaseHelper.connect();
+    await DatabaseHelper.empty(User);
+  });
+
+  afterEach(async () => {
+    await DatabaseHelper.disconnect();
+  });
+
+  describe('POST', () => {
+    it('should create and return a new user [201]', async () => {
+      assert.equal(await User.count(), 0, 'no users saved');
+      await supertest(server)
+        .post('/users')
+        .send({email: 'test@test.com', password: 'test1234'})
+        .expect(201)
+        .expect({email: 'test@test.com'});
+      assert.equal(await User.count(), 1, 'one user saved');
+    });
+    it('should disallow creation of a user when email and password are missing [422]', async () => {
+      assert.equal(await User.count(), 0, 'no users saved');
+      await supertest(server)
+        .post('/users')
+        .expect(422)
+        .expect({
+          code: 'UserNotCreated',
+          message: 'User could not be created.'
+        });
+      assert.equal(await User.count(), 0, 'no users saved');
+    });
+    it('should disallow creation of a user when email is invalid [422]', async () => {
+      assert.equal(await User.count(), 0, 'no users saved');
+      await supertest(server)
+        .post('/users')
+        .send({email: 'test@test.', password: 'test1234'})
+        .expect(422)
+        .expect({
+          code: 'UserNotCreated',
+          message: 'User could not be created.'
+        });
+      assert.equal(await User.count(), 0, 'no users saved');
+    });
+    it('should disallow creation of a user when password is invalid [422]', async () => {
+      assert.equal(await User.count(), 0, 'no users saved');
+      await supertest(server)
+        .post('/users')
+        .send({email: 'test@test.com', password: 'short'})
+        .expect(422)
+        .expect({
+          code: 'UserNotCreated',
+          message: 'User could not be created.'
+        });
+      assert.equal(await User.count(), 0, 'no users saved');
+    });
+    it('should disallow creation of a user with duplicate email [422]', async () => {
+      assert.equal(await User.count(), 0, 'no users saved');
+      await supertest(server)
+        .post('/users')
+        .send({email: 'test@test.com', password: 'test1234'})
+        .expect(201);
+      assert.equal(await User.count(), 1, 'one user saved');
+      await supertest(server)
+        .post('/users')
+        .send({email: 'test@test.com', password: 'test1234'})
+        .expect(422)
+        .expect({
+          code: 'UserNotCreated',
+          message: 'User could not be created.'
+        });
+      assert.equal(await User.count(), 1, 'one user saved');
+    });
+  });
+
+});

8299dea58b95034b67b8475f3b91737f39e8b46c
Randall Morey <randall@randallmorey.com>
2018-03-05 15:16:37 -0500

    wrap route error handling in middleware

--- app/server.js
+++ app/server.js
@@ -2,6 +2,19 @@ import restify from 'restify';
 import { UnprocessableEntityError } from 'restify-errors';
 import User from './models/user';
 
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((e) => {
+    if (e.name === 'ValidationError') {
+      // Pass validation errors as is.
+      next(e);
+    } else {
+      // All errors other than validation errors result in the same response.
+      // This helps to prevent leaking internal server state.
+      next(new UnprocessableEntityError());
+    }
+  });
+};
+
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
@@ -35,26 +48,12 @@ server.on('restifyError', (req, res, err, next) => {
 });
 
 // /users
-server.post('/users', async (req, res, next) => {
-  try {
-    const {email, password} = req.body;
-    const user = await User.create({email, password});
-    // success state
-    res.send(201, {
-      email: user.email
-    });
-    next();
-  } catch (e) {
-    // error state
-    if (e.name === 'ValidationError') {
-      next(e);
-    } else {
-      // All errors other than validation errors result in the same response.
-      // This helps to prevent inadvertantly leaking information about which users
-      // already exist in the database, although it is not foolproof.
-      next(new UnprocessableEntityError());
-    }
-  }
-});
+server.post('/users', asyncMiddleware(async (req, res, next) => {
+  const {email, password} = req.body;
+  const user = await User.create({email, password});
+  res.send(201, {
+    email: user.email
+  });
+}));
 
 export default server;

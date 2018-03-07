f16da81be25858aa1660e453213ac21e155a93ba
Randall Morey <randall@randallmorey.com>
2018-03-07 11:06:35 -0500

    expand jwt service tests

--- test/unit/services/jwt.js
+++ test/unit/services/jwt.js
@@ -12,6 +12,19 @@ describe ('Unit: Service: jwt', () => {
       assert.equal(decoded.sub, 'test@test.com');
       assert.isTrue(decoded.authenticated);
     });
+    it('should allow overriding of `authenticated` value', () => {
+      const token = jwtLoginToken('test@test.com', false);
+      const decoded = jwt.verify(token, JWT_SECRET);
+      assert.isFalse(decoded.authenticated);
+    });
+    it('should allow overriding of `expiresIn` value', () => {
+      const token = jwtLoginToken('test@test.com', true, '-1d');
+      assert.throws(() => {
+        // verify step throws an error when token is expired, demonstrating that
+        // the expiration was indeed overriden
+        jwt.verify(token, JWT_SECRET);
+      }, Error, 'jwt expired');
+    });
   });
 
 });

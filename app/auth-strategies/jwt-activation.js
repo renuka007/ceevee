7bc227be845d45d3f43e92243a85f97f412e2604
Randall Morey <randall@randallmorey.com>
2018-03-15 19:44:03 -0400

    cleanup if style

--- app/auth-strategies/jwt-activation.js
+++ app/auth-strategies/jwt-activation.js
@@ -12,9 +12,7 @@ import { SECURE_KEY } from '../../config/config';
  */
 const jwtActivationAuthStrategy = new BearerStrategy(async (token, next) => {
   const user = await User.findOneAndActivate(token);
-  if (user) {
-    return next(null, user);
-  }
+  if (user) return next(null, user);
   return next(new UnauthorizedError());
 });
 
